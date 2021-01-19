fd0f4337436a3edc7d286ffc167ac18a0d4b1b39
Alisultan Abdullah <47978568+AlisultanAbdullayev@users.noreply.github.com>
2021-01-19 21:37:51 +0300

    Commit

---
+++ Shared/DesignCodeCourseApp.swift
@@ -0,0 +1,18 @@
+//
+//  DesignCodeCourseApp.swift
+//  Shared
+//
+//  Created by Alisultan Abdullah on 1.12.2020.
+//
+
+import SwiftUI
+
+@main
+struct DesignCodeCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
